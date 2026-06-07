26ffa7005cb2e28a5cac6578f71d36707d6072d9
josebasegura-JAS <josebasegura@gmail.com>
2026-06-07 18:48:38 +0200

    Add files via upload

--- src/features/paritaria/components/ParitariaPage.tsx
+++ src/features/paritaria/components/ParitariaPage.tsx
@@ -5,7 +5,13 @@ import { useActasStore } from '../../actas/store/useActasStore';
 import { PARITARIA_SESSION_CONFIG } from '../domain/paritaria';
 import { useParitariaSessionStore } from '../store/useParitariaSessionStore';
 
-export function ParitariaPage() {
+export function ParitariaPage({
+  initialSessionId = null,
+  navigationNonce,
+}: {
+  initialSessionId?: string | null;
+  navigationNonce?: number;
+}) {
   const createActaFromSession = useActasStore((state) => state.createFromSession);
 
   const handleClosedSession = (session: ManagedSession, treatedTasks: Task[]) => {
@@ -15,6 +21,8 @@ export function ParitariaPage() {
   return (
     <SessionManagementPage
       config={PARITARIA_SESSION_CONFIG}
+      initialSessionId={initialSessionId}
+      navigationNonce={navigationNonce}
       onClosedSession={handleClosedSession}
       useSessionStore={useParitariaSessionStore}
     />
