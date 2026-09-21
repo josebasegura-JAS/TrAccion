c2c94470cc1cbf1204d76bc4f2e036121d366812
josebasegura-JAS <josebasegura@gmail.com>
2026-09-21 08:09:25 +0200

    restore src/features/plantilla/domain/employee.ts

--- src/features/plantilla/domain/employee.ts
+++ src/features/plantilla/domain/employee.ts
@@ -1,11 +1,14 @@
 export interface EmployeePersistedFields {
   empleado: string;
   nombreApellidos: string;
-  email: string;
   puestoNomina: string;
   puestoOrganizativo: string;
+  puestoEus: string;
   residencia: string;
+  unidad: string;
   nivelRetributivo: string;
+  direccionOrganizativa: string;
+  antiguedadPuesto: string;
   sexo: string;
   calle: string;
   numero: string;
@@ -14,6 +17,8 @@ export interface EmployeePersistedFields {
   poblacion: string;
   provincia: string;
   nif: string;
+  telefono1: string;
+  telefono2: string;
 }
 
 export interface EmployeeDerivedFields {
@@ -34,11 +39,14 @@ export type EmployeeField = keyof EmployeePersistedFields;
 export const EMPLOYEE_FIELDS: EmployeeField[] = [
   'empleado',
   'nombreApellidos',
-  'email',
   'puestoNomina',
   'puestoOrganizativo',
+  'puestoEus',
   'residencia',
+  'unidad',
   'nivelRetributivo',
+  'direccionOrganizativa',
+  'antiguedadPuesto',
   'sexo',
   'calle',
   'numero',
@@ -47,16 +55,21 @@ export const EMPLOYEE_FIELDS: EmployeeField[] = [
   'poblacion',
   'provincia',
   'nif',
+  'telefono1',
+  'telefono2',
 ];
 
 export const EMPTY_EMPLOYEE_DRAFT: EmployeeDraft = {
   empleado: '',
   nombreApellidos: '',
-  email: '',
   puestoNomina: '',
   puestoOrganizativo: '',
+  puestoEus: '',
   residencia: '',
+  unidad: '',
   nivelRetributivo: '',
+  direccionOrganizativa: '',
+  antiguedadPuesto: '',
   sexo: '',
   calle: '',
   numero: '',
@@ -65,4 +78,6 @@ export const EMPTY_EMPLOYEE_DRAFT: EmployeeDraft = {
   poblacion: '',
   provincia: '',
   nif: '',
+  telefono1: '',
+  telefono2: '',
 };
